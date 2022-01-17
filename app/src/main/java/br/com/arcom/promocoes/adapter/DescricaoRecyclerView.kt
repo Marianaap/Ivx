dfb30e7f6f29fdcd2e94aefc9af4145d04bfdf52
mariana.silva <marianasilvaap1@gmail.com>
2022-01-17 12:48:10 -0300

    Ivx

--- app/src/main/java/br/com/arcom/promocoes/adapter/DescricaoRecyclerView.kt
+++ app/src/main/java/br/com/arcom/promocoes/adapter/DescricaoRecyclerView.kt
@@ -1,29 +1,34 @@
-package com.example.promcoes.adapter
+package br.com.arcom.promocoes.adapter
 
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import br.com.arcom.promocoes.R
 
-
-class Horizontal_RecyclerView: RecyclerView.Adapter<Horizontal_RecyclerView.MyViewHolder>() {
+class DescricaoRecyclerView: RecyclerView.Adapter<DescricaoRecyclerView.MyViewHolder>() {
 
     class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var posicao : TextView
+
+        init {
+            posicao = itemView.findViewById(R.id.txt_numero)
+        }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.row, parent,false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.descricao_promo, parent,false)
         return MyViewHolder(view)
 
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
 
+        holder.posicao.text = position.toString()
 
     }
 
-    override fun getItemCount() = 5
+    override fun getItemCount() = 30
 }
